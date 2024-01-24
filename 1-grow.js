0c0c2e75e3efc0945d9dc2e88214d661bd3b2559
mfiks <fikri.setiawan182@gmail.com>
2024-01-24 11:06:43 +0700

    day 1/365

---
+++ 1-grow.js
@@ -0,0 +1,19 @@
+// Coding challenge 2024
+// 1/365
+// 
+
+// function grow(x){
+//     let result = x[0]
+//     for ( let i = 1; i < x.length; i++) {
+//         result = result * x[i]
+//     }
+
+//     return result;
+// }
+
+function grow(x) {
+    const result = x.reduce((acc, curr) => acc * curr, 1);
+    return result
+}
+
+console.log(grow([1, 2, 3]));
