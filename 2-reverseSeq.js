ff81c7c6ce1c21f1a3bb2c491d8b73976820e17a
mfiks <fikri.setiawan182@gmail.com>
2024-01-24 21:32:40 +0700

    2/365

---
+++ 2-reverseSeq.js
@@ -0,0 +1,20 @@
+// Coding challenge 2024
+// 2/365
+// https://www.codewars.com/kata/5a00e05cc374cb34d100000d/train/javascript
+
+// cara 1
+const reverseSeq = n => {
+    const arr = [];
+    for (let i = n; i >= 1 ; i--){
+        arr.push(i)
+    }    
+    return arr;
+}
+
+// cara 2
+// const reverseSeq = n => [...Array(n)].map((el, i) => i + 1).reverse()
+
+// cara 3
+// const reverseSeq = n => [...Array(n)].map((el, i) => n - i )
+
+console.log(reverseSeq(5))
