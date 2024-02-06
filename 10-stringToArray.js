9d54a3f5ec8371cfe7d930716907be95bacc7433
mfiks <fikri.setiawan182@gmail.com>
2024-02-06 22:06:57 +0700

    11/365

---
+++ 10-stringToArray.js
@@ -0,0 +1,15 @@
+// Coding challenge 2024
+// 10/365
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+// Write a function to split a string and convert it into an array of words.
+// "Robin Singh" ==> ["Robin", "Singh"]
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+// function stringToArray(string){
+//     return string.split(' ')
+// }
+
+const stringToArray = (string) => string.split(' ')
+
+console.log(stringToArray("Robin Singh")) // ['Robin', 'Singh']
---
+++ 11-rps.js
@@ -0,0 +1,35 @@
+// Coding challenge 2024
+// 11/365
+// https://www.codewars.com/kata/5672a98bdbdd995fad00000f
+
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+// const rps = (p1, p2) => {
+//     if (p1 === p2) return 'Draw!'
+//     if (p1 === 'rock' && p2 === 'scissors'){
+//         return 'Player 1 won!'
+//     } else if (p1 === 'scissors' && p2 === 'paper') {
+//         return 'Player 1 won!'
+//     } else if (p1 === 'paper' && p2 === 'rock') {
+//         return 'Player 1 won!'
+//     } else {
+//         return 'Player 2 won!'
+//     }
+// };
+
+const rps = (p1, p2) => {
+    if (p1 === p2) return 'Draw!'
+    if (p1 === 'rock' && p2 === 'scissors' || (p1 === 'scissors' && p2 === 'paper') || (p1 === 'paper' && p2 === 'rock')){
+        return 'Player 1 won!'
+    } else {
+        return 'Player 2 won!'
+    }
+};
+
+console.log(rps('rock', 'scissors'))
+console.log(rps('rock', 'paper'))
